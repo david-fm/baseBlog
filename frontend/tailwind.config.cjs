490e24aebb9ae2457fe23296e940df70dbdf834a
David Florez Mazuera <pandafm200@gmail.com>
2023-09-14 09:41:59 +0200

    Fixed colors and added tags

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,15 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors:{
-			'coral': '#EF8354',
-			'ucla-blue': '#467599',
-			'erie-black': '#28231C',
-			'ghost-white': '#F4F4F9',
-			'thistle': '#E5D0E3'
-
+		extend: {
+			colors:{
+				'coral': '#EF8354',
+				'ucla-blue': '#467599',
+				'erie-black': '#28231C',
+				'ghost-white': '#F4F4F9',
+				'thistle': '#E5D0E3'
+	
+			},
 		},
-		extend: {},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
