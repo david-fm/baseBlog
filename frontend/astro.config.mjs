d16b4ac264c5dfa71a5cc2584b6add3e170342cc
David Florez Mazuera <pandafm200@gmail.com>
2023-09-14 09:50:32 +0200

    Added support for mdx files
    Allowing to add personalized css to text

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ["picsum.photos"]
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   markdown: {
-    syntaxHighlight: false,
-  },
+    syntaxHighlight: false
+  }
 });
