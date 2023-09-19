23bb69d648612ba642269817802125bb50cb3bbc
David Florez Mazuera <pandafm200@gmail.com>
2023-09-19 20:48:25 +0200

    Delete django backend

--- backend/images/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Image
-
-@admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
-    '''Images will be available on the address domain/media/images/name_of_image'''
-    pass
