616d26ef89e24f55e8df567f452cdc0508a09d7d
David Florez Mazuera <pandafm200@gmail.com>
2023-09-27 22:57:41 +0200

    Functional programming post

--- frontend/src/components/Caption.tsx
+++ frontend/src/components/Caption.tsx
@@ -1,15 +1,15 @@
 interface Props {
     children: any;
     url: string;
+    alt: string;
 }
 
-export default function Caption({ children, url }: Props) {
+export default function Caption({ children, url, alt }: Props) {
     return (
     <div className="w-full flex flex-col items-center justify-center">
         
-        <img src={url} alt="alt de la imagen" class="rounded-lg shadow-xl object-cover object-center m-0">
+        <img src={url} alt={alt} class="rounded-lg shadow-xl object-cover object-center m-0">
         </img>
-
         <p className="text-xs text-erie-black text-center">{children}</p>
     </div>);
     }
